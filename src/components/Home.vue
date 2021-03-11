d4b6ad9335bd3ed247a5b3f9b80de2321dc0dede
wesley93 <1124700977@qq.com>
2021-03-11 11:38:20 +0800

    主页-实现基本布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="home-container">
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container ">
+<!--    头部区域  -->
+    <el-header>
+      <div class="logo">
+        <img src="../assets/logo.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+<!--    页面主体区域  -->
+    <el-container>
+<!--      侧边栏-->
+      <el-aside width="200px">Aside</el-aside>
+<!--      右侧内容主体  -->
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -25,6 +39,34 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color:#666;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 10px;
+  align-items: center;
+  height: 60px;
+  .logo {
+    display: flex;
+    align-items: center;
+    span{
+      color: #fff;
+      font-size: 16px;
+      margin-left: 15px;
+    }
+    img {
+      height: 60px;
+    }
+  }
+}
+.el-aside {
+  background-color:#333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  pages: {
+    index: {
+      // page 的入口
+      entry: 'src/main.js',
+      // 模板来源
+      template: 'public/index.html',
+      // 在 dist/index.html 的输出
+      filename: 'index.html',
+      title: 'vue管理系统'
+    }
+  },
   css: {
     loaderOptions: {
       less: {
